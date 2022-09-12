809c85d19488a2ed34f34c786014cdb452720e1b
nguyendinhnguyen2001 <90900772+nguyendinhnguyen2001@users.noreply.github.com>
2022-09-12 16:47:30 +0700

    Update FAST_API.py

--- FAST_API.py
+++ FAST_API.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import pickle
+import numpy as np
 app=FastAPI()
 filename='diabetes.sav'
 model = pickle.load(open(filename, 'rb'))
@@ -9,7 +10,4 @@ def predict(BMI:int,Age:int,Glucose:int):
     features_list = [BMI,Age,Glucose]
     prediction = model.predict([features_list])
     confidence = model.predict_proba([features_list])
-    response = {}
-    response['prediction'] = int(prediction[0])
-    response['confidence'] = str(round(np.amax(confidence[0]) * 100 ,2))
-    return response
+    return {"Prediction":int(prediction[0]),"Confidence":str(round(np.amax(confidence[0]) * 100 ,2))}
